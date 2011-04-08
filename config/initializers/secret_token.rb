ce40f68b68d5118895d162ea9ba83fdfa30245ad
drsnuggles <PhilippBay@gmail.com>
2011-04-08 19:16:48 +1000

    and off we go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Euromap::Application.config.secret_token = '444427e5e3e3a37df88ab0e3f7f5926cffef2f6d2922431ce8e10276acdcbd79e640176aaa66ade96d36abc8edc4ea4677a1f623a6c6e09bb22cc720c3e1c37c'
