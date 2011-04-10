e03008c1f5e6a71c8dea6883d17b450ea834b48e
drsnuggles <PhilippBay@gmail.com>
2011-04-10 21:11:05 +1000

    small bugfix in europe.js, still too large

---
+++ minimal/parseJS.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys
+
+toparse = open(sys.argv[1],"r")
+print "var tiger = [0,0,600,600"
+for line in toparse:
+	line = line.replace("\n","")
+	if "type" in line:
+		# Cut off beginning of line and end of line, replace by own stuff
+		line = line.replace("\n","") # for second \n
+		line = line.replace("g.createPath(",",") # that should take care of the beginning
+		# the following should take care of the end
+		line = line.replace(line[line.find("}"):],',"stroke-width":"0.172",stroke:"#000",fill:"#fff"},')
+	print line
