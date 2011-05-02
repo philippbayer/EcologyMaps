766b2ffdb19ed8c4f3bcd91a6d1ada6213f2588d
Bastian Greshake <bgreshake@googlemail.com>
2011-05-02 22:10:12 +0200

    added picture-getter

---
+++ data/picture_getter.py
@@ -0,0 +1,12 @@
+import sys
+import commands
+infile = open(sys.argv[1],"r")
+lines = infile.readlines()
+
+fixlines = []
+
+for i in lines:	
+	newline = i.split(",")
+	if newline[2].find("CC") != -1 or newline[2].find("PD") != -1:
+		print newline[2]
+		commands.getstatusoutput("curl "+newline[1]+" > pictures/"+newline[0].replace(" ","_")+".jpeg")
