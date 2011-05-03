790160659334f6734083cf7b178260b6f98c3ab2
drsnuggles <PhilippBay@gmail.com>
2011-05-03 22:43:57 +1000

    Some parser for years + imagemaps, years.csv has results - deleted unfindable imagemaps, original imagemaps in data/uncleanYears

---
+++ data/html2csv.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+myoutputfile = File.new("years.csv","a+")
+directory = Dir.new(Dir.pwd)
+# iterate over contents of working directory
+directory.each do |d|
+	if d.include? "20"
+		# now open the file and parse through it
+		yearname = d[0,4]
+		yearstring = ""
+        toparse = File.open(d,"r")
+		# go through each line
+		toparse.each_line do |line|
+			# does it have the desirable html-tags?
+			if line.include?("<img") or line.include?("<map") or line.include?("<area") or line.include?("</img") or line.include?("</map") or line.include?("</area")
+				# replace pesky linebreaks
+				line = line.sub("\n","") + " "
+				yearstring += line
+			end
+		end
+		# write output, ; as delimiter
+		myoutputfile.write(yearname + ";" + yearstring + "\n")
+		toparse.close()
+	end
+end
+myoutputfile.close()
