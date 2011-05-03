73c7694e499dd15562dfa12220b5af35a72bbc98
drsnuggles <PhilippBay@gmail.com>
2011-05-03 14:17:36 +1000

    Put main into years - doesnt work so far haha

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -4,4 +4,17 @@ class YearsController < ApplicationController
       format.html # index.html.erb
     end
   end
+
+  def Two004
+	respond_to do |format|
+	  format.html
+	end
+  end
+
+  def main
+	respond_to do |format|
+	  format.html
+	end
+  end
+
 end
