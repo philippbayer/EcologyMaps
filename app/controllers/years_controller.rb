187b1f82d80d157e2d6694c31800bbaf998e819a
drsnuggles <PhilippBay@gmail.com>
2011-05-03 16:57:29 +1000

    Added more year-links

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -4,17 +4,4 @@ class YearsController < ApplicationController
       format.html # index.html.erb
     end
   end
-
-  def Two004
-	respond_to do |format|
-	  format.html
-	end
-  end
-
-  def main
-	respond_to do |format|
-	  format.html
-	end
-  end
-
 end
